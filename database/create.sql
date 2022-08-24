4398aa7c6acb379751a6aee798dbf33fefaf43e6
Leandro Ferreira <leandrosf934@gmail.com>
2022-08-24 20:23:23 -0300

    Inicio da implementação do JWT para autenticação do usuário

--- database/create.sql
+++ database/create.sql
@@ -1,15 +1,35 @@
 create schema pokemon
 
+/* Crianção de enum para o campo role */
+create type pokemon.enum_role as enum(
+	'user', 
+	'admin', 
+	'moderator'
+)
+
+create table pokemon.roles(
+	id uuid primary key,
+	name pokemon.enum_role not null,
+	date timestamp default now() not null
+)
+
+create table pokemon.user_roles(
+	id uuid primary key,
+	role_id uuid not null,
+	user_id uuid not null,
+	date timestamp default now() not null
+)
+
 create table pokemon.users(
-    id text primary key,
+    id uuid primary key,
     name text not null,
     password text not null,
     email text not null,
-    date timestamp default now()
+    date timestamp default now() not null
 )
 
 create table pokemon.pokemons(
-    id text primary key,
+    id uuid primary key,
     pokeapi_id text not null,
     name text not null,
     hp integer not null,
@@ -23,20 +43,32 @@ create table pokemon.pokemons(
     sprite text not null,
     ability_one text,
     ability_two text,
-    date timestamp default now()
+    date timestamp default now() not null
 )
 
 create table pokemon.user_pokemons(
-    id text primary key, 
-    user_id text not null,
-    pokemon_id text not null,
-    date timestamp default now()
+    id uuid primary key, 
+    user_id uuid not null,
+    pokemon_id uuid not null,
+    date timestamp default now() not null
 )
 
+/*CONSTRAINT - user_id e user_pokemons */
 alter table pokemon.user_pokemons 
 add constraint pokemons_user_id_fk 
 foreign key (user_id) references pokemon.users(id)
 
+/*CONSTRAINT - pokemon_id e user_pokemons */
 alter table pokemon.user_pokemons 
 add constraint pokemons_pokemon_id_fk 
 foreign key (pokemon_id) references pokemon.pokemons(id)
+
+/*CONSTRAINT - role_id e user_roles */
+alter table pokemon.user_roles 
+add constraint roles_role_id_fk 
+foreign key (role_id) references pokemon.roles(id)
+
+/*CONSTRAINT - user_id e user_roles */
+alter table pokemon.user_roles 
+add constraint roles_user_id_fk 
+foreign key (user_id) references pokemon.users(id)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const express = require ('express');
+const express = require('express');
 const router = require('./route/router');
-const app = express ();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
 
-app.use(express.json());
+app.use(cors({ origin: `http://localhost:3030` }));
 
-const port = 3000
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', router)
 
+const port = 3000;
 
 app.listen(port, () => console.log(`servidor ligado http://localhost:${port}/api`))
